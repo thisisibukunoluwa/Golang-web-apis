270b4fb8ab26dc0f1fe34238da4c9329934bbe49
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2024-01-12 09:08:47 +0100

    feat:added and modified files

--- coffeester/services/coffee.go
+++ coffeester/services/coffee.go
@@ -52,6 +52,77 @@ func (c *Coffee) GetAllCoffees() ([]*Coffee, error) {
 	return coffees, nil 
 }
 
+func (c *Coffee)GetCoffeeById(id string) (*Coffee, error) {
+	ctx,cancel := context.WithTimeout(context.Background(),dbTimeout)
+	defer cancel()
+	query := `SELECT id, name, image, roast, region, price, grind_unit, updated_at 
+	FROM coffees
+	WHERE id = $1
+	`
+	var coffee Coffee
+
+	row := db.QueryRowContext(ctx,query, id)
+	err := row.Scan(
+			&coffee.ID,
+			&coffee.Name,
+			&coffee.Image,
+			&coffee.Roast,
+			&coffee.Region,
+			&coffee.Price,
+			&coffee.GrindUnit,
+			&coffee.CreatedAt,
+			&coffee.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil , err
+	}
+
+	return &coffee, nil 
+}
+
+func (c *Coffee) UpdateCoffee(id string, body Coffee) (*Coffee, error) {
+	ctx,cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `
+		UPDATE coffees 
+		SET
+			name = $1
+			image = $2
+			roast = $3
+			price = $4
+			grind_unit = $5
+			region = $6
+			updated_at = $7
+		WHERE id = $8
+		returning *
+	`
+	_, err := db.ExecContext(
+		ctx,query,
+		body.Name,
+		body.Image,
+		body.Roast,
+		body.Price,
+		body.GrindUnit,
+		body.Region,
+		time.Now(),
+		id,
+	)
+	if err != nil {
+		return nil,err 
+	}
+
+	return &body, nil 
+}
+
+func (c *Coffee) DeleteCoffee(id string) error {
+	
+}
+
+
+
+
 func (c *Coffee) CreateCoffee(coffee Coffee) (*Coffee, error) {
 	ctx, cancel := context.WithTimeout(context.Background(),dbTimeout)
 	defer cancel()
